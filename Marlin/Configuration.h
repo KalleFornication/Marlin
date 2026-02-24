539b756a821d9d3d1de232851c8230288c5fc28d
sebastian <s@ebast.ian>
2026-02-24 10:57:42 +0100

    adapted extruder steps per mm from 357 to 500

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1335,7 +1335,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 80, 357 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 80, 500 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
